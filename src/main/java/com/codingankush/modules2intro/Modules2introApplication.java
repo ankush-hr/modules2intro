e2c76552437cfffd170ef1f8e481b3e90423d978
ankush-hr <siwachankush2004@gmail.com>
2025-11-27 21:52:20 +0530

    project for modules 2

---
+++ src/main/java/com/codingankush/modules2intro/Modules2introApplication.java
@@ -0,0 +1,13 @@
+package com.codingankush.modules2intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modules2introApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modules2introApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingankush/modules2intro/Modules2introApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingankush.modules2intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modules2introApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
