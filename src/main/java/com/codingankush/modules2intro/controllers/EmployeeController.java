37e4402b017525aba7a95f733bc0d870bfd455c2
ankush-hr <siwachankush2004@gmail.com>
2025-11-28 19:06:26 +0530

    adding more files

--- src/main/java/com/codingankush/modules2intro/controllers/EmployeeController.java
+++ src/main/java/com/codingankush/modules2intro/controllers/EmployeeController.java
@@ -2,24 +2,39 @@ package com.codingankush.modules2intro.controllers;
 
 import com.codingankush.modules2intro.dto.EmployeeDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path = "/employees")
 public class EmployeeController {
 //    @GetMapping("/hi")
 //    public String home(String hii) {
 //        return "Hello Ankush, Spring Boot is running!";
 //    }
 
-    @GetMapping("/employees/{employeeId}")
-    public EmployeeDto getEmployeeById( @PathVariable long employeeId){
-        return new EmployeeDto(employeeId , "Ankush","ankush@gmail.com",21 ,
+    @GetMapping(path = "/{employeeId}")
+    public EmployeeDto getEmployeeById( @PathVariable(name = "employeeId") long Id){
+        return new EmployeeDto(Id , "Ankush","ankush@gmail.com",21 ,
                 LocalDate.of(2025 ,11 ,28), true );
 
+    }
+    @GetMapping
+    public  String getname (@RequestParam(required = false, name = "inputAge") Integer age,
+                            @RequestParam(required = false ) String sortBy) {
+        return "hi my age is "+ age + sortBy;
 
+    }
+
+    @PostMapping
+    public EmployeeDto  createNewNewEmployee(@RequestBody EmployeeDto inputEmployee ){
+        inputEmployee.setId(100L);
+        return inputEmployee;
+    }
+
+    @PutMapping
+    public String updateEmployeeByid(){
+        return "I am from put";
     }
 }
